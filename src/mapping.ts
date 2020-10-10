0d40ceb245bae781655ed4cdbb3c425607d1421e
0x2b543c <ncmaddrey@gmail.com>
2020-10-10 13:29:00 -0400

    fix type conversions & build

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,13 +1,13 @@
 import { CardAdded, Staked, Withdrawn} from '../generated/Sven/Sven'
 import { TransferSingle} from '../generated/MemeLtd/MemeLtd'
 import { Stake, User, NFT, Card } from '../generated/schema'
-import { Address, log } from '@graphprotocol/graph-ts'
+import { Address, Bytes, BigInt, log } from '@graphprotocol/graph-ts'
 
 export function handleCardAdded(event: CardAdded): void {
   let id = event.params.card
-  let card = new Card(id)
+  let card = new Card(id.toHex())
   card.pineappleCost = event.params.points
-  card.pool = event.transaction.to
+  card.pool = event.transaction.to as Bytes
   card.save()
 }
 
@@ -15,21 +15,21 @@ export function handleStaked(event: Staked): void {
   let id = event.transaction.hash.toHex() + "-" + event.logIndex.toString()
   let stake = new Stake(id)
   getOrCreateUser(event.params.user)
-  stake.user = event.params.user
+  stake.user = event.params.user.toHex()
   stake.amount = event.params.amount
-  stake.timestamp = event.block.timetstamp
-  stake.pool = event.transaction.to
+  stake.timestamp = event.block.timestamp
+  stake.pool = event.transaction.to as Bytes
   stake.save()
 }
 
 export function handleWithdrawn(event: Withdrawn): void {
   let id = event.transaction.hash.toHex() + "-" + event.logIndex.toString()
   let stake = new Stake(id)
-  stake.user = event.params.user
+  stake.user = event.params.user.toHex()
   /// Unstake event amounts are represented as negative balances 
-  stake.amount = event.params.amount * -1
-  stake.timestamp = event.block.timetstamp
-  stake.pool = event.transaction.to
+  stake.amount = -(event.params.amount as BigInt)
+  stake.timestamp = event.block.timestamp
+  stake.pool = event.transaction.to as Bytes
   stake.save()
 }
 
@@ -38,10 +38,10 @@ export function handleTransferSingle(event: TransferSingle): void {
   let id = event.transaction.hash.toHex() + "-" + event.logIndex.toString()
   let nft = new NFT(id)
   let card_id = event.params._id
-  let card = Card.load(card_id)
-  nft.mintedBy = event.params._to
+  let card = Card.load(card_id.toString())
+  nft.mintedBy = event.params._to.toHex()
   nft.mintedTimestamp = event.block.timestamp
-  nft.card = card_id
+  nft.card = card_id.toString()
   nft.save()
 }
 
